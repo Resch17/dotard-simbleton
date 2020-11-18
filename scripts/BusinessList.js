11c2aae3aa5e404b77f66a9093e373e104f378c3
Aaron Resch <aaronmresch@gmail.com>
2020-11-18 15:43:16 -0600

    did NY filter

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -2,7 +2,7 @@ import {Business} from "./Business.js";
 import {useBusinesses} from "./BusinessProvider.js"
 
 export const BusinessList = () => {
-  const domTarget = document.querySelector(".container")
+  const domTarget = document.querySelector(".businessList")
   const businessArray = useBusinesses();
 
   businessArray.forEach((business) => {
@@ -10,4 +10,19 @@ export const BusinessList = () => {
     }
   )
 
+}
+
+export const NewYorkList = () => {
+  const businessArray = useBusinesses();
+  const nyElement = document.querySelector(".businessList--newYork")
+  let newYorkArray = businessArray.filter(business => {
+    if (business.addressStateCode === "NY") {
+      return true;
+    }
+    return false;
+  });
+
+  newYorkArray.forEach((business) => {
+    nyElement.innerHTML += Business(business);
+  });
 }
